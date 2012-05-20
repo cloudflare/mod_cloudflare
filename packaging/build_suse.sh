736a2424cfe581445e0858ce4de66ebd76cf13bf
Ray Bejjani <ray@cloudflare.com>
2012-05-19 18:32:10 -0700

    build_suse.sh

---
+++ packaging/build_suse.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+HOST=root@$1
+
+git archive --format=tar --prefix='mod_cloudflare/' HEAD -o mod_cloudflare.tar.gz
+scp -i mod_cloudflare.keypair mod_cloudflare.tar.gz $HOST: 
+
+ssh -i mod_cloudflare.keypair $HOST 'tar -xf mod_cloudflare.tar.gz; cd mod_cloudflare/packaging && make pkg-rpm-system-prep-suse; useradd rpm; chown -R rpm:users /root; su -p rpm -c "cd mod_cloudflare/packaging && make pkg-rpm-suse"'
+scp -i mod_cloudflare.keypair $HOST:/root/mod_cloudflare/rpm/RPMS/* . 
