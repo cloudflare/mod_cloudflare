f5e812a2e57727cf96d363f21f3b0288b00979ec
p0358 <p0358@users.noreply.github.com>
2018-11-18 23:39:17 +0100

    Did some proper verification of results before writing to config file
    As suggested by @AndreiG6
    also on this other branch (soon should delete it I guess)

--- mod_cloudflare_conf_ip_update.php
+++ mod_cloudflare_conf_ip_update.php
@@ -14,15 +14,30 @@ $arr = [];
 
 $ipv4 = file_get_contents('https://www.cloudflare.com/ips-v4');
 $ipv6 = file_get_contents('https://www.cloudflare.com/ips-v6');
+if (empty($ipv4) || empty($ipv6)) {
+    fwrite(STDERR, "Error: script could not download the latest IP ranges from Cloudflare\n");
+    exit(1);
+}
 
 $ipv4_arr = explode("\n", trim($ipv4));
-foreach ($ipv4_arr as $line) {
-    $arr[] = trim($line);
+foreach ($ipv4_arr as &$line) {
+    // Source: https://www.regextester.com/98096
+    if (preg_match('/^((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2])))$/', trim($line), $matches)) {
+        $arr[] = $matches[1];
+    }
 }
 
 $ipv6_arr = explode("\n", trim($ipv6));
-foreach ($ipv6_arr as $line) {
-    $arr[] = trim($line);
+foreach ($ipv6_arr as &$line) {
+    // Source: https://www.regextester.com/93988
+    if (preg_match('/^s*(((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:)))(%.+)?s*(\/([0-9]|[1-9][0-9]|1[0-1][0-9]|12[0-8]))?)$/', trim($line), $matches)) {
+        $arr[] = $matches[1];
+    }
+}
+
+if (empty($arr)) {
+    fwrite(STDERR, "Error: script got 0 results while trying to get the latest IP ranges from Cloudflare, terminating\n");
+    exit(2);
 }
 
 //$arr[] = ''; -- you can append your public server's IP(s) here too
